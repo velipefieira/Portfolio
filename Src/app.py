6098a7bd88b4b458099363cce5b9b2d6be758dfe
Felipe Vieira <felipe.vieira30@fatec.sp.gov.br>
2023-04-12 08:52:50 -0300

    Criando arquivo home.html e app.py

---
+++ Src/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    titulo = "Home"
+    return render_template ("home.html", title = titulo)
+
+@app.route("/home.html")
+def home2():
+    titulo = "Home"
+    return render_template ("home.html", title = titulo)
+
+@app.route("/escolaridade.html")
+def escola():
+    titulo = "Escolaridade"
+    return render_template ("escolaridade.html", title = titulo)
+
+@app.route("/hobbies")
+def hobbies():
+    titulo = "Hobbies"
+    return render_template ("hobbies.html", title = titulo)
